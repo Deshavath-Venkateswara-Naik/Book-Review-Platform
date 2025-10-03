f9872c30424d554e92a9802f0d787376522d99d6
Deshavath-Venkateswara-Naik <deshavathvenkateswaranaik0193@gmail.com>
2025-10-03 23:09:31 +0530

    deployed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+// Updated base URL to deployed Render backend
+const API = axios.create({ baseURL: 'https://book-review-platform-3.onrender.com/api' });
 
 // Attach token to headers
 API.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
-    if(token) config.headers.Authorization = `Bearer ${token}`;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
 });
 
